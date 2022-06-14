1fc85f9d7214a9915a51f94ebacfb5fa5b686ba9
andrewrgarcia <andrewrgarcia@ufl.edu>
2022-06-14 14:30:50 -0400

    solved hello.cpp

---
+++ google/hello.cpp
@@ -0,0 +1,19 @@
+// hello.cpp: Andrew
+// Description: "hello world" from Google c++
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    for (int j = 0; j < 6; j++)
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            cout << "Hello World! ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
