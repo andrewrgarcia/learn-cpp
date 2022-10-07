5be8a04910bf3ed81a549d3604cf18622529f933
andrewrgarcia <andrewrgarcia@ufl.edu>
2022-10-07 18:34:30 -0400

    add some comments

--- top10/3__Dynamic-Programming/edit-distance/edist_DP-memo--4.cpp
+++ top10/3__Dynamic-Programming/edit-distance/edist_DP-memo--4.cpp
@@ -13,9 +13,8 @@ int minDis(string s1, string s2, int n, int m,
     if (m == 0)
         return n;
 
-    // To check if the recursive tree
-    // for given n & m has already been executed
-
+    // IF DP VALUE HAS ALREADY BEEN CHANGED ONCE, DO NOT RUN AGAIN JUST RETURN
+    // *THEY ALL START AS -1 
     if (dp[n][m] != -1)
         return dp[n][m];
 
@@ -26,13 +25,13 @@ int minDis(string s1, string s2, int n, int m,
     {
         return dp[n][m] = minDis(s1, s2, n - 1, m - 1, dp);
     }
-    // If characters are nt equal, we need to
+    // If characters are not equal, we need to
     // find the minimum cost out of all 3 operations.
     // 1. insert 2.delete 3.replace
     else
     {
         int insert, del, replace; // temp variables
-
+        // SAME ALGORITHM AS ALL OTHER METHODS FOR EDIT-DISTANCE
         insert = minDis(s1, s2, n, m - 1, dp);
         del = minDis(s1, s2, n - 1, m, dp);
         replace = minDis(s1, s2, n - 1, m - 1, dp);
@@ -43,12 +42,12 @@ int minDis(string s1, string s2, int n, int m,
 // Driver program
 int main()
 {
-
+    // HAGRID PORTER
     string str1 = "voldemort";
     string str2 = "dumbledore";
 
     int n = str1.length(), m = str2.length();
-    vector<vector<int>> dp(n + 1, vector<int>(m + 1, -1));
+    vector<vector<int>> dp(n + 1, vector<int>(m + 1, -1)); // MATRIX FILLED WITH -1 ENTRIES (INIT)
 
     cout << minDis(str1, str2, n, m, dp);
     return 0;
