2193fcab87f60a4d23b651c2ff98e5e9a484e161
andrewrgarcia <andrewrgarcia@ufl.edu>
2022-10-26 00:14:10 -0400

    make way less verbose

--- top10/5__Binary-Tree/remove-leaves-k.cpp
+++ top10/5__Binary-Tree/remove-leaves-k.cpp
@@ -25,25 +25,18 @@ Node *removeShortPathNodesUtil(Node *root, int level, int k)
     if (root == NULL)
         return NULL;
 
-    // Traverse the tree in postorder fashion so that if a leaf
-    // node path length is shorter than k, then that node and
-    // all of its descendants till the node which are not
-    // on some other path are removed.
+    // Traverse the tree ON BOTH BRANCHES RECURSIVELY
     root->left = removeShortPathNodesUtil(root->left, level + 1, k);
     root->right = removeShortPathNodesUtil(root->right, level + 1, k);
 
-    // If root is a leaf node and it's level is less than k then
-    // remove this node.
-    // This goes up and check for the ancestor nodes also for the
-    // same condition till it finds a node which is a part of other
-    // path(s) too.
+    // IF THE NEXT LEAVES ARE NULL [I.E. END OF THE TREE] AND
+    // THE DEPTH IS LESS THAN K, DELETE LEAF:
     if (root->left == NULL && root->right == NULL && level < k)
     {
         delete root;
         return NULL;
     }
 
-    // Return root;
     return root;
 }
 
