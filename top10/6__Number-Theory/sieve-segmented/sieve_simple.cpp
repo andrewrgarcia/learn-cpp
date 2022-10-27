b2d03562a0884a3b8e6e7d7e6be595cf1e26c20c
andrewrgarcia <andrewrgarcia@ufl.edu>
2022-10-27 16:51:47 -0400

    segmented sieve complete

--- top10/6__Number-Theory/sieve-segmented/sieve_simple.cpp
+++ top10/6__Number-Theory/sieve-segmented/sieve_simple.cpp
@@ -9,25 +9,26 @@ void simpleSieve(int limit)
 	// in mark[p] will finally be false if 'p' is Not
 	// a prime, else true.
 	bool mark[limit];
-	for(int i = 0; i<limit; i++) {
-	mark[i] = true;
+	for (int i = 0; i < limit; i++)
+	{
+		mark[i] = true;
 	}
 
 	// One by one traverse all numbers so that their
 	// multiples can be marked as composite.
-	for (int p=2; p*p<limit; p++)
+	for (int p = 2; p * p < limit; p++)
 	{
 		// If p is not changed, then it is a prime
 		if (mark[p] == true)
 		{
 			// Update all multiples of p
-			for (int i=p*p; i<limit; i+=p)
+			for (int i = p * p; i < limit; i += p)
 				mark[i] = false;
 		}
 	}
 
 	// Print all prime numbers and store them in prime
-	for (int p=2; p<limit; p++)
+	for (int p = 2; p < limit; p++)
 		if (mark[p] == true)
 			cout << p << " ";
 }
